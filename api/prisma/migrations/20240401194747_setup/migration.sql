9f9f15f9a19c2a812efda699e9fb0c09904f8d6b
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-02 16:05:32 -0300

    fix prisma migration file to postgres container;

--- api/prisma/migrations/20240401194747_setup/migration.sql
+++ api/prisma/migrations/20240401194747_setup/migration.sql
@@ -9,7 +9,7 @@ CREATE TABLE "events" (
 
 -- CreateTable
 CREATE TABLE "attendees" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" serial NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
